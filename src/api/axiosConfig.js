e3e2b52d369e7d2b79f2feca8b43562098e6eaea
KALIDBEYAN <86565904+KALIDBEYAN@users.noreply.github.com>
2026-03-29 23:40:34 +0300

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://akaki-backend-1.onrender.com",
+  baseURL: "https://akaki-backend-1.onrender.com/api",
   withCredentials: true
 });
 
